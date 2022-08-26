e6aafc1073d6b9fa86372afbe275e51319c099fc
Lloyd Fernandez <lloydfernandez15@gmail.com>
2022-08-26 10:18:36 +0800

    feat: add capstone backend

--- ExpressExercisesB/suspiciousactivity/index.js
+++ ExpressExercisesB/suspiciousactivity/index.js
@@ -19,16 +19,16 @@ app.use(bodyParser.json())
 app.use('/', router)
 
 const getActualRequestDurationInMilliseconds = start => {
-    const NS_PER_SEC = 1e9; //  convert to nanoseconds
-    const NS_TO_MS = 1e6; // convert to milliseconds
-    const diff = process.hrtime(start);
-    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+    const NS_PER_SEC = 1e9 //  convert to nanoseconds
+    const NS_TO_MS = 1e6 // convert to milliseconds
+    const diff = process.hrtime(start)
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
   };
   
   
 let demoLogger = (req, res, next) => { //middleware function
-let current_datetime = new Date();
-let formatted_date =
+    let current_datetime = new Date();
+    let formatted_date =
     current_datetime.getFullYear() +
     "-" +
     (current_datetime.getMonth() + 1) +
@@ -39,36 +39,37 @@ let formatted_date =
     ":" +
     current_datetime.getMinutes() +
     ":" +
-    current_datetime.getSeconds();
+    current_datetime.getSeconds() +
+    ":" + 
+    current_datetime.toDateString() 
     
-let method = req.method;
-let url = req.url;
-let status = res.statusCode;
-const start = process.hrtime();
-const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
-let log = `[${formatted_date}] ${method}:${url} ${status} ${durationInMilliseconds.toLocaleString()} ms`;
-console.log(log);
-fs.appendFile("log.txt", log + "\n", err => {
-    if (err) {
-    console.log(err);
-    }
-});
-next();
-};
+    let method = req.method
+    let url = req.url
+    let status = res.statusCode
+    const start = process.hrtime()
+    const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
+    let log = `[${formatted_date}] ${method}:${url} ${status} ${durationInMilliseconds.toLocaleString()} ms`;
+    console.log(log)
 
-  
+    fs.appendFile("log.txt", log + "\n", err => {
+        if (err) {
+        console.log(err);
+        }
+    });
+    next()
+}
+
 app.use(demoLogger);
   
-  
 app.get("/", (req, res) => {
-res.send("This is the home Page");
-});
+    res.send("This is the home Page")
+})
 app.get("/about", (req, res) => {
-res.send("This is the About page");
-});
+    res.send("This is the About page")
+})
 app.post("/add", (req, res) => {
-res.send("Post request");
-});
+    res.send("Post request")
+})
 app.listen(PORT, () => {
-console.log(`App is listening to port ${PORT}`);
-});
+    console.log(`App is listening to port ${PORT}`)
+})
